b80d30a6f11043594162f07960d3764a1e5200e8
Ahmed <borcheni.ahmed99@gmail.com>
2025-04-02 18:25:21 +0200

    adding comments, reverting greyimage code

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,25 @@ use image::GrayImage;
 use image::Luma;
 use image::RgbImage;
 use image::Rgb;
-use proj4rs;
-use proj4rs::proj::Proj;
-use colorgrad::Gradient;
+use std::env;
 
 fn get_data(content: &str, params: &mut GridParams) -> Vec<Vec<f32>> {
     // getting the grid parameters
     let mut lines = content.lines();
+    // the 6 first lines have the paramters
     for _ in 0..6 {
         if let Some(line) = lines.next() {
             let mut parts = line.split_whitespace();
             match parts.next() {
-                Some("xllcenter") => params.x_begin = parts.next().and_then(|s| s.parse::<f64>().ok()),
-                Some("yllcenter") => params.y_begin = parts.next().and_then(|s| s.parse::<f64>().ok()),
-                Some("cellsize") => params.cellsize = parts.next().and_then(|s| s.parse::<f64>().ok()),
-                Some("nodata_value") => params.nodata = parts.next().and_then(|s| s.parse::<f64>().ok()),
+                Some("cellsize") => params.cellsize = parts.next().and_then(|s| s.parse::<f32>().ok()),
+                Some("nodata_value") => params.nodata = parts.next().and_then(|s| s.parse::<f32>().ok()),
                 _ => continue,
             }
         }
     }
-
+    // the rest of the lines has the data, extract it to a matrix form
     lines.map(|line| {
+        // data values are split by whitespaces
         line.split_whitespace()
             .filter_map(|s| s.parse::<f32>().ok())
             .collect()
@@ -33,7 +31,7 @@ fn get_data(content: &str, params: &mut GridParams) -> Vec<Vec<f32>> {
 }
 
 fn find_min(grid: &Vec<Vec<Option<f32>>>) -> Option<f32> {
-
+    // iterate through the matrix and get the minimum value
     grid.iter()
         .flatten()
         .filter_map(|&x| x)
@@ -42,44 +40,22 @@ fn find_min(grid: &Vec<Vec<Option<f32>>>) -> Option<f32> {
 }
 
 fn find_max(grid: &Vec<Vec<Option<f32>>>) -> Option<f32> {
-    let mut max_val_x = 0.0;
-    let mut max_val_y = 0.0;
-    let mut max_elevation = 0.0;
-
+    // iterate through the matrix and get the maximum value
     grid.iter()
         .flatten()
         .filter_map(|&x| x)
         .reduce(f32::max)
 }
 
-fn find_max_position(grid: &Vec<Vec<Option<f32>>>) -> Option<(usize, usize, f32)> {
-    let mut max_pos = None;
-    
-    for (row_idx, row) in grid.iter().enumerate() {
-        for (col_idx, &val) in row.iter().enumerate() {
-            if let Some(val) = val {
-                match max_pos {
-                    Some((_, _, current_max)) if val > current_max => {
-                        max_pos = Some((row_idx, col_idx, val));
-                    }
-                    None => {
-                        max_pos = Some((row_idx, col_idx, val));
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-    
-    max_pos
-}
 
-
-fn replace_nodata(grid: &Vec<Vec<f32>>) -> Vec<Vec<Option<f32>>> {
+fn replace_nodata(grid: &Vec<Vec<f32>>, nodata_val:Option<f32>) -> Vec<Vec<Option<f32>>> {
+    // the nodata value is passed, otherwise the default is -99999.0
+    let nodata = nodata_val.unwrap_or(-99999.0);
+    // iterate through the matrix and if the value is -99999.0 (using float comparision), put None
     grid.iter()
         .map(|row| {
             row.iter()
-                .map(|&x| if (x + 99999.0).abs() < 0.0001 { None } else { Some(x) }) // Replace NaNs
+                .map(|&x| if (x - nodata).abs() < f32::EPSILON { None } else { Some(x) }) // Replace NaNs
                 .collect()
         })
         .collect()
@@ -93,6 +69,7 @@ fn compute_hillshade(
     azimuth: f32,
     altitude: f32,
 ) -> f32 {
+    // get the dimensions
     let height = grid.len();
     let width = if height > 0 { grid[0].len() } else { 0 };
 
@@ -134,28 +111,32 @@ fn compute_hillshade(
 }
 
 
-fn grid_to_colored_image(grid: &Vec<Vec<Option<f32>>> )->RgbImage {
+fn grid_to_colored_image(grid: &Vec<Vec<Option<f32>>>,cellsize:Option<f32> )->(RgbImage, RgbImage) {
+    // get image dimensions
     let height = grid.len();
     let width = if height > 0 { grid[0].len() } else { 0 };
 
     let mut image = RgbImage::new(width as u32, height as u32);
 
-    // Light parameters (adjust these to change lighting)
+    // Light parameters default values
     let azimuth = 315.0f32.to_radians(); // Light direction (NW)
     let altitude = 45.0f32.to_radians(); // Sun angle
-    let cell_size=1.0;
+    // get the cellsize value for the hillshade algorithm, 1.0 is the default value
+    let cell_size = cellsize.unwrap_or(1.0);
     // Find min and max values ignoring None
     let max_depth = find_min(&grid);
     let min_depth = find_max(&grid);
 
+    // if there is no valid data points, return
     let (max_val, min_val) = match (max_depth, min_depth) {
         (Some(max), Some(min)) => (max, min),
         _ => {
-            eprintln!("No valid data points. Returning blank image.");
-            return image;
+            eprintln!("No valid data points. Returning blank images.");
+            return (image.clone(),image);
         }
     };
 
+    // loop through all the points and assign pixel values
     for (y, row) in grid.iter().enumerate() {
         for (x, value_opt) in row.iter().enumerate() {
             let pixel = match value_opt {
@@ -164,18 +145,18 @@ fn grid_to_colored_image(grid: &Vec<Vec<Option<f32>>> )->RgbImage {
                     let t = if (max_val - min_val).abs() < f32::EPSILON {
                         0.5
                     } else {
+                        // normalize the values, to have something in the [0,1] range
                         (value - min_val) / (max_val - min_val)
                     };
                     
                     // Convert to color gradient (blue -> green -> red)
                     let (r, g, b) = hsl_to_rgb(
-                        240.0 * (t),  // Hue from 240° (blue) to 0° (red)
+                        240.0 * (t),  // Hue from 240° (blue) to 0° (red), map [0,1] range to [0,240]
                         0.8,                // Full saturation
                         0.5                 // Medium lightness
                     );
                     // Calculate hillshade
                     let hillshade = compute_hillshade(&grid, y, x, cell_size, azimuth, altitude);
-                    // Rgb([r, g, b])
 
                     // Combine color with hillshade
                     Rgb([
@@ -190,33 +171,77 @@ fn grid_to_colored_image(grid: &Vec<Vec<Option<f32>>> )->RgbImage {
         }
     }
 
-    // Draw a black "X" every 50 cells
+    //clone the image to add the feature
+    let mut x_marked_image = image.clone();    // Draw a black "X" every 50 cells
     for y in (0..height).step_by(20) {
         for x in (0..width).step_by(20) {
             let radius = 2; // Size of the "X"
             for offset in 0..=radius {
                 // Draw the diagonal from top-left to bottom-right
                 if x + offset < width && y + offset < height {
-                    image.put_pixel((x + offset) as u32, (y + offset) as u32, Rgb([0, 0, 0])); // Black
+                    x_marked_image.put_pixel((x + offset) as u32, (y + offset) as u32, Rgb([0, 0, 0])); // Black
                 }
                 if x >= offset && y >= offset {
-                    image.put_pixel((x - offset) as u32, (y - offset) as u32, Rgb([0, 0, 0])); // Black
+                    x_marked_image.put_pixel((x - offset) as u32, (y - offset) as u32, Rgb([0, 0, 0])); // Black
                 }
 
                 // Draw the diagonal from top-right to bottom-left
                 if x >= offset && y + offset < height {
-                    image.put_pixel((x - offset) as u32, (y + offset) as u32, Rgb([0, 0, 0])); // Black
+                    x_marked_image.put_pixel((x - offset) as u32, (y + offset) as u32, Rgb([0, 0, 0])); // Black
                 }
                 if x + offset < width && y >= offset {
-                    image.put_pixel((x + offset) as u32, (y - offset) as u32, Rgb([0, 0, 0])); // Black
+                    x_marked_image.put_pixel((x + offset) as u32, (y - offset) as u32, Rgb([0, 0, 0])); // Black
                 }
             }
 
             // Compute gradient and draw dashed line
-            draw_dashed_line(&mut image, &grid, x, y, cell_size);
+            draw_dashed_line(&mut x_marked_image, &grid, x, y, cell_size);
         }
     }
 
+    return (image,x_marked_image);
+}
+
+fn grid_to_image(grid: &Vec<Vec<Option<f32>>> )->GrayImage {
+    // get image dimensions
+    let height = grid.len();
+    let width = if height > 0 { grid[0].len() } else { 0 };
+
+    let mut image = GrayImage::new(width as u32, height as u32);
+
+    let max_depth = find_min(&grid);
+    let min_depth = find_max(&grid);
+
+    let (max_val, min_val) = match (max_depth, min_depth) {
+        (Some(max), Some(min)) => (max, min),
+        _ => {
+            eprintln!("No valid data points. Returning blank image.");
+            return image;
+        }
+    };
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, value_opt) in row.iter().enumerate() {
+            let pixel_value = match value_opt {
+                Some(value) => {
+                    // Handle case where all values are the same
+                    let range = max_val - min_val;
+                    let normalized = if range.abs() < f32::EPSILON {
+                        0.0
+                    } else {
+                        // normalize the values
+                        (value - min_val) / range
+                    };
+                    let inverted = 1.0 - normalized;
+                    // clip to [0, 255]
+                    (inverted * 255.0).clamp(0.0, 255.0) as u8
+                }
+                None => 0u8, // No-data values are black
+            };
+
+            image.put_pixel(x as u32, y as u32, Luma([pixel_value]));
+        }
+    }
     return image;
 }
 
@@ -314,64 +339,15 @@ fn hsl_to_rgb(h: f32, s: f32, l: f32) -> (u8, u8, u8) {
 }
 
 
-
-fn get_matrix_dimensions(matrix: &Vec<Vec<f32>>) -> (usize, usize) {
-    let rows = matrix.len();
-    let cols = matrix.first().map_or(0, |row| row.len());
-    (rows, cols)
-}
-
-// fn transform_coordinates(grid: &Vec<Vec<f32>>)-> Vec<Vec<f32>> {
-fn transform_coordinates(rows_len:usize , cols_len:usize, cellsize:f64 ){
-
-
-    let lambert93 = Proj::from_proj_string(
-        "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-    ).unwrap();
-
-    let wgs84 = Proj::from_proj_string("+proj=longlat +datum=WGS84 +no_defs").unwrap();
-    // Example: Convert a point (X, Y) in Lambert-93 to (Lat, Lon) in WGS84
-    let xllcenter = 929000.0;  // Example X coordinate (meters)
-    let yllcenter = 6224001.0; // Example Y coordinate (meters)
-
-    // Calculate points (now using f64)
-    let mut points = [
-        (xllcenter, yllcenter),                          // origin
-        (xllcenter + (rows_len as f64) * cellsize, yllcenter),  // top
-        (xllcenter, yllcenter + (cols_len as f64) * cellsize),  // right
-        (xllcenter + (rows_len as f64) * cellsize,       // top-right
-         yllcenter + (cols_len as f64) * cellsize)
-    ];
-
-    // Transform all points
-    for point in &mut points {
-        proj4rs::transform::transform(&lambert93, &wgs84, point).unwrap();
-        // Convert radians to degrees
-        point.0 = point.0.to_degrees();
-        point.1 = point.1.to_degrees();
-    }
-
-    // Print results
-    println!("Origin: Longitude: {:.6}, Latitude: {:.6}", points[0].0, points[0].1);
-    println!("Top: Longitude: {:.6}, Latitude: {:.6}", points[1].0, points[1].1);
-    println!("Right: Longitude: {:.6}, Latitude: {:.6}", points[2].0, points[2].1);
-    println!("Top right: Longitude: {:.6}, Latitude: {:.6}", points[3].0, points[3].1);
-
-}
-
 #[derive(Debug)]
 struct GridParams {
-    x_begin: Option<f64>, 
-    y_begin: Option<f64>,  
-    cellsize: Option<f64>,
-    nodata: Option<f64>,
+    cellsize: Option<f32>,
+    nodata: Option<f32>,
 }
 
 impl Default for GridParams {
     fn default() -> Self {
         Self {
-            x_begin: None,
-            y_begin: None,
             cellsize: None,
             nodata:None,
         }
@@ -379,7 +355,19 @@ impl Default for GridParams {
 }
 
 fn main() {
-    let file_path = Path::new("/home/ahmed/Desktop/2nd_semester/RUST_COURSE/project/data/0925_6225/LITTO3D_FRA_0927_6223_20150529_LAMB93_RGF93_IGN69/MNT1m/LITTO3D_FRA_0927_6223_MNT_20150529_LAMB93_RGF93_IGN69.asc");
+    // Collect command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if a file path was provided
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+    println!("Processing file: {}", file_path);
+    
+    let file_path = Path::new(file_path);
     // Initialize to None
     let mut grid: Option<Vec<Vec<f32>>> = None; 
     let mut params = GridParams::default(); 
@@ -387,33 +375,25 @@ fn main() {
     match fs::read_to_string(file_path) {
         Ok(content) => {
             grid = Some(get_data(content.as_str(),&mut params));
-            println!("Params: {:?}", params);
-        },
-        
+            println!("These are the parameters from the data: {:?}", params);
+        }, 
         Err(e) => eprintln!("Failed to read data file: {}", e), // Print error to stderr
     }
 
-    // check if match has a value
+    // check if grid has values
     match grid {
         // Some(image) => println!("Transformed content: {:?}", image),
         Some(grid_data) =>
         {
             // replace the -99999 value (or any other nodata value) with None
-            let transformed_grid = replace_nodata(&grid_data);
-
-            let (rows, cols) = get_matrix_dimensions(&grid_data);
-            println!("Rows: {}, Columns: {}", rows, cols);  // Output: Rows: 2, Columns: 3
-            if let Some((row, col, max_val)) = find_max_position(&transformed_grid) {
-                println!("Max value {} at row {}, column {}", max_val, row, col);
-                    transform_coordinates(rows,cols,1.0);
-                }
-                // Output: Max value 4.8 at row 2, column 0
-            else {
-                println!("Grid contains no valid values");
-            }
+            let transformed_grid = replace_nodata(&grid_data,params.nodata);
             
-            let img_col = grid_to_colored_image(&transformed_grid);
+            //save the greyscale image
+            let img = grid_to_image(&transformed_grid);
+            img.save("greyscale.png").unwrap();
+            let (img_col,img_feature) = grid_to_colored_image(&transformed_grid,params.cellsize);
             img_col.save("colored.png").unwrap();
+            img_feature.save("gradient.png").unwrap();
 
         }
         None => println!("No image data available."),
